d9878e3da56c6eb01b2ffff9e34c5f0ba7c26002
Mpineiro20 <130186047+Mpineiro20@users.noreply.github.com>
2023-08-26 00:31:20 +0000

    first cimmit

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,53 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+    return {
+        store: {
+            favs: [],
+            characters: [],
+            vehicles: [],
+            planets: [],
+        },
+        actions: {
+            addRemoveFav: (newFav) => {
+                const { favs } = getStore();
+                if (!favs.includes(newFav)) {
+                    setStore({ favs: [...favs, newFav] });
+                } else {
+                    const newList = favs.filter((el) => el !== newFav);
+                    setStore({ favs: newList });
+                }
+            },
+            getCharacterData: async () => {
+                const resp = await fetch("https://www.swapi.tech/api/people");
+                const data = await resp.json();
+                setStore({ characters: data.results });
+                console.log(data.results);
+            },
+            getCharacterDetails: async (uid) => {
+                const resp = await fetch("https://www.swapi.tech/api/people/" + uid);
+                const data = await resp.json();
+                setStore({ characterDetails: data.results });
+                console.log(data.result);
+            },
+            getVehiclesData: async () => {
+                const resp = await fetch("https://www.swapi.tech/api/starships");
+                const data = await resp.json();
+                setStore({ vehicles: data.results });
+                console.log(data.results);
+            },
+            getVehicleDetails: async (uid) => {
+                const resp = await fetch("https://www.swapi.tech/api/starships/" + uid);
+                const data = await resp.json();
+                setStore({ vehicleDetails: data });
+                console.log(data);
+            },
+            getPlanetsData: async () => {
+                const resp = await fetch("https://www.swapi.tech/api/planets");
+                const data = await resp.json();
+                setStore({ planets: data.results });
+                console.log(data.results);
+            },
+        },
+    };
 };
 
 export default getState;
